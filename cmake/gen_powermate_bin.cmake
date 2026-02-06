3f8e38b7791dc6d85a82325b8b55a143f8f83275
YoungSoo Shin <shinys000114@gmail.com>
2026-02-06 11:02:37 +0900

    update: modify output filenames in `gen_powermate_bin.cmake` to include `powermate-` prefix
    Signed-off-by: YoungSoo Shin <shinys000114@gmail.com>
    Change-Id: I77282d8c221cffb8afdc9cf26c2485a5522b4c35

--- cmake/gen_powermate_bin.cmake
+++ cmake/gen_powermate_bin.cmake
@@ -1,8 +1,8 @@
 if (NOT TARGET powermate_gen_single_bin)
     add_custom_target(
             powermate_gen_single_bin
-            COMMAND ${CMAKE_COMMAND} -E echo "Merge bin files to ${PROJECT_VER}.bin"
-            COMMAND ${ESPTOOLPY} --chip ${IDF_TARGET} merge_bin -o ${PROJECT_VER}.bin @flash_args
+            COMMAND ${CMAKE_COMMAND} -E echo "Merge bin files to powermate-${PROJECT_VER}.bin"
+            COMMAND ${ESPTOOLPY} --chip ${IDF_TARGET} merge_bin -o powermate-${PROJECT_VER}.bin @flash_args
             COMMAND ${CMAKE_COMMAND} -E echo "Merge bin done"
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             DEPENDS gen_project_binary bootloader
@@ -10,12 +10,12 @@ if (NOT TARGET powermate_gen_single_bin)
     )
 endif()
 
-# Flash bin ${PROJECT_VER}.bin to target chip
+# Flash bin powermate-${PROJECT_VER}.bin to target chip
 if (NOT TARGET powermate_flash_single_bin)
     add_custom_target(
             powermate_flash_single_bin
-            COMMAND ${CMAKE_COMMAND} -E echo "Flash merged bin ${PROJECT_VER}.bin to address 0x0"
-            COMMAND ${ESPTOOLPY} --chip ${IDF_TARGET} write_flash 0x0 ${PROJECT_VER}.bin
+            COMMAND ${CMAKE_COMMAND} -E echo "Flash merged bin powermate-${PROJECT_VER}.bin to address 0x0"
+            COMMAND ${ESPTOOLPY} --chip ${IDF_TARGET} write_flash 0x0 powermate-${PROJECT_VER}.bin
             COMMAND ${CMAKE_COMMAND} -E echo "Flash merged bin done"
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             DEPENDS powermate_gen_single_bin
